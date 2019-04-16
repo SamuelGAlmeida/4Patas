21f471ee92445005de2205f9bc02a16e28b76292
Samuel Almeida <samuel_gomes_almeida@hotmail.com>
2019-04-16 12:27:02 +0100

    4rd commit (verificarDia)

--- src/package01/Date.java
+++ src/package01/Date.java
@@ -4,34 +4,74 @@ import enums.Meses;
 
 public class Date {
 
-    public int day;
-    public Meses mes;
-    public int year;
+    private int day;
+    private Meses mes;
+    private int year;
 
     Date(int day, Meses month, int year) {
 
-        this.mes = month;
+        if (verificarDia(day, month)) {
+            this.mes = month;
+            this.year = year;
+        }
+        this.day = 1;
+        this.mes = Meses.JAN;
+        this.year = 2000;
 
-        if (day < 32 && day > 0) {
+    }
 
+    private boolean verificarDia(int dia, Meses mes) {
+        if (dia < 32 && dia > 0) {
+
+            //Se o mês for Fevereiro os dias são entre 1-29
             if (mes == Meses.FEV) {
-                if (day >= 0 && day <= 29) {
-                    this.day = day;
+                if (dia >= 0 && dia <= 29) {
+                    return true;
                 }
             }
 
-            if (mes == Meses.ABR || mes == Meses.JUN || mes == Meses.SET || mes == Meses.NOV) {
-                if (day > 0 && day <= 30) {
-                    this.day = day;
+            //Se forem os meses Abril, Junho, Setembro, Novembro
+            //os dias têm de ser entre 1-30
+            if (mes == Meses.ABR || mes == Meses.JUN
+                    || mes == Meses.SET || mes == Meses.NOV) {
+                if (dia > 0 && dia <= 30) {
+                    return true;
                 }
             }
-
-            this.day = day;
+            return true;
         } else {
-            System.out.println("Número de dia inválido");
+            return false;
         }
 
-        this.year = year;
+    }
 
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        if (verificarDia(day, this.mes)) {
+            this.day = day;
+        }
+        System.out.println("Dia inválido para o mês selecionado");
+    }
+
+    public Meses getMes() {
+        return mes;
+    }
+
+    public void setMes(Meses mes) {
+        if (verificarDia(this.day, mes)) {
+            this.mes = mes;
+        }
+        System.out.println("Mês inválido para o dia selecionado");
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 }
---
+++ src/package01/Reserva.java
@@ -0,0 +1,6 @@
+package package01;
+
+public class Reserva {
+
+    
+}
